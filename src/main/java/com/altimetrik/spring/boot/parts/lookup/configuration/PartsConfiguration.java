261da1d51c21c598b11a2d0e077dfd53c90a2537
Abhishek Basukala <abhishekbasukala@gmail.com>
2021-01-10 22:55:47 -0500

    Added Swagger Configurations

---
+++ src/main/java/com/altimetrik/spring/boot/parts/lookup/configuration/PartsConfiguration.java
@@ -0,0 +1,23 @@
+package com.altimetrik.spring.boot.parts.lookup.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class PartsConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.altimetrik.spring.boot"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
--- src/main/java/com/altimetrik/spring/boot/parts/lookup/exception/PartsControllerAdvice.java
+++ src/main/java/com/altimetrik/spring/boot/parts/lookup/exception/PartsControllerAdvice.java
@@ -2,7 +2,6 @@ package com.altimetrik.spring.boot.parts.lookup.exception;
 
 import com.altimetrik.spring.boot.parts.lookup.model.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
